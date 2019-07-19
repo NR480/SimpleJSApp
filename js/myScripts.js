3c47a0bf1464f3a1fea8bd36ef1d50d471a33fec
Natasha <47771734+NR480@users.noreply.github.com>
2019-07-19 13:37:45 -0700

    Changed an array and loop
    Changed array of arrays into an array of objects and for loop into a forEach loop.

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,49 +1,57 @@
-//var dogRepository = [
-// [name: 'Baxter', age: 2, height: 16, ['brown', 'Basenji']],
-// [name: 'Pokemon', age: 8, height: 26, ['black', 'Doberman']],
-//[name: 'Lucy', age: 15, height: 10, ['grey', 'Maltese']],
-//];
+//Create a var called repository and assign it to be an array.
+// Add several objects to the array. Add three keys and make up a value for each.
 
-
-var dogRepository = [
-    ['Baxter',
-        16, ['brown', 'Basenji']
-    ],
-
-
-    ['Pokemon',
-        26, ['black', 'Doberman']
-    ],
-
-    ['Lucy',
-        10, ['grey', 'Maltese']
-    ],
-]
-
-
-console.log(dogRepository[1][0]);
-
-for (var i = 0; i < dogRepository.length; i++) {
-    if (repository[i][1] > 16) {
-        document.write('<p class="top_article">Wow, this dog is the biggest!</p>');
+var dogRepository = [{
+        name: 'Baxter',
+        height: 16,
+        type: ['brown', 'Basenji']
+    },
+    {
+        name: 'Pokemon',
+        height: 26,
+        type: ['black', 'Doberman']
+    },
+    {
+        name: 'Lucy',
+        height: 10,
+        type: ['grey', 'Maltese']
     }
-    document.write('<h2><a href="">' + dogRepository[i][0] + '</a></h2>');
+];
 
+console.log(dogRepository[0][2]);
 
-    document.write('<p class="p_small">Type: ');
-    for (var j = 0; j < dogRepository[i][2].length; j++) {
+//Create a for loop and  make it forEach( for each item of the repository).
+//Use document.write() inside the loop's code to write  on the website's DOM.
+//Adding strings, write the dog's height next to its name.
+/* Within a loop, add a conditional that cheks if the height is above a certain vakue.
+If it is, add the note "Wow, this dog is big!" to the output. Only one dog should have this label.*/
 
-        if (dogRepository[i][2][j] == 'brown') {
-            document.write(' <span class="p__small" style="color:rgb(255, 69, 0);">' + repository[i][2][j] + ',');
-        } else if (dogRepository[i][2][j] == 'Basenji') {
-            document.write(' <span class="p__small" style="color:rgb(255, 69, 0);">' + repository[i][2][j] + ',');
-        } else if (dogRepository[i][2][j] == 'black') {
-            document.write(' <span class="p__small" style="color:rgb(255, 69, 0);">' + repository[i][2][j] + ',');
-        } else if (dogRepository[i][2][j] == 'Doberman') {
-            document.write(' <span class="p__small" style="color:rgb(255, 69, 0);">' + repository[i][2][j] + ',');
+dogRepository.forEach(dog => {
+    console.log(dog.name, dog.height, dog.type);
+    document.write(dog.name, dog.height, dog.type);
+
+    for (var i = 0; i < dogRepository.length; i++) {
+        if (dogRepository[i][1] > 16) {
+            document.write('<p class="top article">Wow, this dog is the biggest!</p>');
         }
 
+
     }
-    document.write('</p>');
-    document.write('<BR><BR>');
+});
+
+document.write('<p class="p_small" Type:');
+for (var i = 0; i < dogRepository[i][2].length; i++) {
+    if (dogRepository[i][2][i] == 'brown') {
+        document.write('<span class="p_small">');
+        style = "color: rgb(255, 69,0);" > ' + repository[i] [2] [i] + ', ')'
+    } else if (dogRepository[i][2][i] == 'Basenji') {
+        document.write('<span class="p_small">');
+        style = "color: rgb(255, 69, 0);" > ' + repository[i] [2] [i] + ', ')'
+    } else if (dogRepository[i][2][i] == 'black') {
+        document.write('<span class="p_small">');
+        style = "color: rgb(255,69,0);" > ' + repository[i] [2] [i] + ', ')'
+    } else if (dogRepository[i][2][i] == 'Doberman') {
+        document.write('<span class="p_small">');
+        style = "color: rgb(255,69,0);" > ' + repository[i] [2] [i] + ', ')'
+    }
 }
